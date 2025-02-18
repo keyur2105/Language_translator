0e457371e953b3e1389dc8c3e9c45f17c48905b9
keyur2105 <keyurnakrani901@gmail.com>
2025-02-18 15:31:10 +0530

    murder

--- Language_translator/views.py
+++ Language_translator/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from googletrans import Translator
 
 def translator(request):
-    
+
     if request.method=="GET":
 
         text = request.GET.get("text","").strip()
+        print(text)
         language = request.GET.get("language")
-
         if not text or not language:
             return render(request,"index.html", {"error":"All Feild Required.."})
         
         translate = Translator()
         translate_text = translate.translate(text,dest=language).text
-        return render(request, "index.html", {'translate_text':translate_text})
+        return render(request, "index.html", {'translate_text':translate_text, 'text':text,})
     
     return render(request, "index.html")
